860cf560e1f3ff48e6ce443cfb99f1da3251230f
Xavier Loera Flores <59235228+xavierloeraflores@users.noreply.github.com>
2020-04-02 17:39:29 -0700

    Added Javascript File

---
+++ Website/script.js
@@ -0,0 +1,4 @@
+
+$.getJSON("https://my.callofduty.com/api/papi-client/stats/cod/v1/title/mw/platform/battle/gamer/iShot%2321899/profile/friends/type/mp", function(data){
+    console.log(data);
+});
