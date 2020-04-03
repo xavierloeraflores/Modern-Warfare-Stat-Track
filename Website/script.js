eac77e4cfc5b348246cef41224d749ecd2352197
Xavier Loera Flores <59235228+xavierloeraflores@users.noreply.github.com>
2020-04-02 19:00:59 -0700

    updated script.js & added Guns.txt
    JSON data for SOULDASYTHE now appears on the page successfully. 
    Added a txt file that shows the gun file names from game to their actual in-game names

--- Website/script.js
+++ Website/script.js
@@ -1,4 +1,6 @@
-
-$.getJSON("https://my.callofduty.com/api/papi-client/stats/cod/v1/title/mw/platform/battle/gamer/iShot%2321899/profile/friends/type/mp", function(data){
-    console.log(data);
-});
+$.getJSON(
+    "https://my.callofduty.com/api/papi-client/stats/cod/v1/title/mw/platform/battle/gamer/souldasythe%231176/profile/type/mp",
+    function(data) {
+        console.log(data);
+    }
+);
